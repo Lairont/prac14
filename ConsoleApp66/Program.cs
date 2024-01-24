ec8d1c77f55e1bc24c7e59688b92e8cc5db072d9
Никита <153456515+Lairont@users.noreply.github.com>
2024-01-24 19:58:49 +0300

    Add files via upload

---
+++ ConsoleApp66/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание1 
+            //int x = 1; 
+            //while (x <= 10) 
+            //{ 
+            // Console.WriteLine(x); 
+            // x+=1; 
+            //} 
+            //Console.ReadLine(); 
+            //задание2 //Console.Write("n = "); 
+            //int n = int.Parse(Console.ReadLine()); 
+            //int x = 1; //while (x <= n) 
+            //{ 
+            // Console.WriteLine(x); 
+            // x += 1; 
+            //} 
+            //Console.ReadLine(); 
+            //задание3 //int x = 1; 
+            //while (x <= 5) 
+            //{ 
+            // Console.WriteLine("Привет!"); 
+            // x += 1; 
+            //} //Console.ReadLine(); 
+            //задание4 (а)
+            //int x = 1;
+            //while (x <= 18)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.Write("#");
+            //    x += 1;
+            //}
+            //Console.ReadLine();
+            //задание4 (б)
+            //int x = 1;
+            //while (x <= 25)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Yellow;
+            //    Console.WriteLine("#");
+            //    x += 1;
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
